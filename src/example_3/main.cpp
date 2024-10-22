e8a67678a6460a836ebabe072b65d6cc3d5044d1
smdaa <saadmdaa03@gmail.com>
2024-10-22 22:24:15 +0200

    example 3

--- src/example_3/main.cpp
+++ src/example_3/main.cpp
@@ -1,340 +1,190 @@
-#include "cinder/CinderImGui.h"
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/ip/Resize.h"
 
 #include <immintrin.h>
 #include <omp.h>
+#include <queue>
 
-using namespace ci;
-using namespace ci::app;
+constexpr int screenWidth = 1920;
+constexpr int screenHeight = 1080;
+constexpr double constantX = -0.391;
+constexpr double constantY = -0.587;
+constexpr double scale = 1.0;
+constexpr double offsetX = 0.0;
+constexpr double offsetY = 0.0;
+constexpr int maxIterations = 1000;
+constexpr int escapeRadiusSquared = 4;
 
-struct fractalConfiguration
-{
-    std::string constantName;
-    double constantX;
-    double constantY;
-    double offsetX;
-    double offsetY;
-    double scale;
-    int maxIterations;
-    double escapeRadiusSquared;
-};
-
-struct screenConfiguration
-{
-    int width;
-    int height;
-};
-
-const std::map<std::string, std::pair<double, double>> fractalConstants = {
-    {"San Marco", {-0.75, 0.0}},
-    {"Douady's Rabbit", {-0.123, 0.745}},
-    {"Siegel Disk", {-0.391, -0.587}},
-    {"Feigenbaum Point", {-1.401155, 0.0}},
-    {"Misiurewicz Point", {0.0, 1.0}},
-};
-
-const fractalConfiguration defaultFractalConfiguration = {"Dendrite", fractalConstants.at("Douady's Rabbit").first, fractalConstants.at("Douady's Rabbit").second, 0.0, 0.0, 1.0, 100, 4.0};
-
-const screenConfiguration defaultScreenConfiguration = {1920, 1080};
-
-__m256i computeJulia(__m256d positionX, __m256d positionY,
-                     __m256d constantX, __m256d constantY,
-                     __m256d escapeRadiusSquared, int maxIterations)
-{
-    __m256d x = positionX;
-    __m256d y = positionY;
-    __m256i iterations = _mm256_setzero_si256();
-    __m256i ones = _mm256_set1_epi64x(1);
-    __m256i maxIter = _mm256_set1_epi64x(maxIterations);
-    for (int i = 0; i < maxIterations; ++i)
-    {
-        __m256d x2 = _mm256_mul_pd(x, x);
-        __m256d y2 = _mm256_mul_pd(y, y);
-        __m256d xy = _mm256_mul_pd(x, y);
-        __m256d mag2 = _mm256_add_pd(x2, y2);
-        __m256d mask = _mm256_cmp_pd(mag2, escapeRadiusSquared, _CMP_LT_OQ);
-        if (_mm256_movemask_pd(mask) == 0)
-            break;
-        __m256d newX = _mm256_add_pd(_mm256_sub_pd(x2, y2), constantX);
-        y = _mm256_add_pd(_mm256_add_pd(xy, xy), constantY);
-        x = newX;
-        iterations = _mm256_add_epi64(iterations, _mm256_and_si256(ones, _mm256_castpd_si256(mask)));
-    }
-    return iterations;
+__m256i computeJulia(__m256d positionX, __m256d positionY, __m256d constantX,
+                     __m256d constantY, __m256d escapeRadiusSquared,
+                     int maxIterations) {
+  __m256d x = positionX;
+  __m256d y = positionY;
+  __m256i iterations = _mm256_setzero_si256();
+  __m256i ones = _mm256_set1_epi64x(1);
+  __m256i maxIter = _mm256_set1_epi64x(maxIterations);
+  for (int i = 0; i < maxIterations; ++i) {
+    __m256d x2 = _mm256_mul_pd(x, x);
+    __m256d y2 = _mm256_mul_pd(y, y);
+    __m256d xy = _mm256_mul_pd(x, y);
+    __m256d mag2 = _mm256_add_pd(x2, y2);
+    __m256d mask = _mm256_cmp_pd(mag2, escapeRadiusSquared, _CMP_LT_OQ);
+    if (_mm256_movemask_pd(mask) == 0)
+      break;
+    __m256d newX = _mm256_add_pd(_mm256_sub_pd(x2, y2), constantX);
+    y = _mm256_add_pd(_mm256_add_pd(xy, xy), constantY);
+    x = newX;
+    iterations = _mm256_add_epi64(
+        iterations, _mm256_and_si256(ones, _mm256_castpd_si256(mask)));
+  }
+  return iterations;
 }
 
-Surface32f renderFractal(const fractalConfiguration &fractalConfig, const screenConfiguration &screenConfig)
-{
-    Surface32f surface(screenConfig.width, screenConfig.height, false);
+void renderJulia(ci::Surface32f *surface, int screenWidth, int screenHeight,
+                 double constantX, double constantY, double offsetX,
+                 double offsetY, double scale, int maxIterations,
+                 double escapeRadiusSquared) {
 #pragma omp parallel for
-    for (int y = 0; y < screenConfig.height; ++y)
-    {
-        for (int x = 0; x < screenConfig.width; x += 4)
-        {
-            __m256d posX = _mm256_set_pd(
-                (x + 3 - screenConfig.width / 2.0) / (screenConfig.width / 2.0f) / fractalConfig.scale + fractalConfig.offsetX,
-                (x + 2 - screenConfig.width / 2.0) / (screenConfig.width / 2.0f) / fractalConfig.scale + fractalConfig.offsetX,
-                (x + 1 - screenConfig.width / 2.0) / (screenConfig.width / 2.0f) / fractalConfig.scale + fractalConfig.offsetX,
-                (x - screenConfig.width / 2.0) / (screenConfig.width / 2.0f) / fractalConfig.scale + fractalConfig.offsetX);
+  for (int y = 0; y < screenHeight; ++y) {
+    for (int x = 0; x < screenWidth; x += 4) {
+      double screenWidthHalf = screenWidth / 2.0;
+      double scaleInv = 1.0 / scale;
+      __m256d screenWidthHalfVec = _mm256_set1_pd(screenWidthHalf);
+      __m256d scaleInvVec = _mm256_set1_pd(scaleInv);
+      __m256d xVec = _mm256_set_pd(x + 3, x + 2, x + 1, x);
+      __m256d xShifted = _mm256_sub_pd(xVec, screenWidthHalfVec);
+      __m256d posX =
+          _mm256_add_pd(_mm256_mul_pd(_mm256_mul_pd(xShifted, scaleInvVec),
+                                      _mm256_set1_pd(1.0 / screenWidthHalf)),
+                        _mm256_set1_pd(offsetX));
+      __m256d posY = _mm256_set1_pd(
+          (y - screenHeight / 2.0) / screenWidthHalf * scaleInv + offsetY);
 
-            __m256d posY = _mm256_set1_pd(
-                (y - screenConfig.height / 2.0) / (screenConfig.width / 2.0f) / fractalConfig.scale + fractalConfig.offsetY);
+      __m256d constX = _mm256_set1_pd(constantX);
+      __m256d constY = _mm256_set1_pd(constantY);
+      __m256d escRadSq = _mm256_set1_pd(escapeRadiusSquared);
 
-            __m256d constX = _mm256_set1_pd(fractalConfig.constantX);
-            __m256d constY = _mm256_set1_pd(fractalConfig.constantY);
-            __m256d escRadSq = _mm256_set1_pd(fractalConfig.escapeRadiusSquared);
+      __m256i iterations =
+          computeJulia(posX, posY, constX, constY, escRadSq, maxIterations);
 
-            __m256i iterations = computeJulia(posX, posY, constX, constY, escRadSq, fractalConfig.maxIterations);
+      int64_t iterCounts[4];
+      _mm256_storeu_si256((__m256i *)iterCounts, iterations);
 
-            int64_t iterCounts[4];
-            _mm256_storeu_si256((__m256i *)iterCounts, iterations);
-
-            for (int i = 0; i < 4; ++i)
-            {
-                if (x + i < screenConfig.width)
-                {
-                    Color color = Color(iterCounts[i] / (float)fractalConfig.maxIterations, 0.0f, 0.0f);
-                    surface.setPixel(ivec2(x + i, y), color);
-                }
-            }
+      for (int i = 0; i < 4; ++i) {
+        if (x + i < screenWidth) {
+          float brightness = static_cast<float>(iterCounts[i]) /
+                             static_cast<float>(maxIterations);
+          ci::Color color = ci::Color(brightness, brightness, brightness);
+          surface->setPixel(ci::ivec2(x + i, y), color);
         }
+      }
     }
-
-    return surface;
+  }
 }
 
-class FractalApp : public App
-{
+class Renderer {
 public:
-    void setup() override;
-    void update() override;
-    void draw() override;
-    void keyDown(ci::app::KeyEvent event) override;
-    void resize() override;
+  Renderer() : isRunning(true), scale(1.0), zoomSpeed(0.1) {
+    surface = ci::Surface32f(screenWidth, screenHeight, false);
+    worker = std::thread(&Renderer::workerLoop, this);
+  }
+  ~Renderer() {
+    isRunning = false;
+    if (worker.joinable()) {
+      worker.join();
+    }
+  }
 
-    void mouseDown(ci::app::MouseEvent event) override;
-    void mouseDrag(ci::app::MouseEvent event) override;
-    void mouseWheel(ci::app::MouseEvent event) override;
-    void mouseUp(ci::app::MouseEvent event) override;
+  void getFrame(ci::Surface32f &outSurface, double requested_scale) {
+    std::lock_guard<std::mutex> lock(queueMutex);
+
+    if (queue.empty()) {
+      return;
+    }
+
+    while (queue.size() > 1) {
+      queue.pop();
+    }
+
+    double frame_scale = queue.front().first;
+    const ci::Surface32f &frame = queue.front().second;
+    double zoomFactor = requested_scale / frame_scale;
+    int originalWidth = frame.getWidth();
+    int originalHeight = frame.getHeight();
+    float aspectRatio =
+        static_cast<float>(originalWidth) / static_cast<float>(originalHeight);
+    int cropWidth = static_cast<int>(originalWidth / zoomFactor);
+    int cropHeight = static_cast<int>(cropWidth / aspectRatio);
+    int cropX = (originalWidth - cropWidth) / 2;
+    int cropY = (originalHeight - cropHeight) / 2;
+    ci::Area cropArea(cropX, cropY, cropX + cropWidth, cropY + cropHeight);
+    ci::Surface32f croppedSurface = frame.clone(cropArea);
+    outSurface = ci::Surface32f(screenWidth, screenHeight, frame.hasAlpha(),
+                                frame.getChannelOrder());
+    ci::ip::resize(croppedSurface, &outSurface);
+  }
 
 private:
-    screenConfiguration mScreenConfiguration = defaultScreenConfiguration;
-    fractalConfiguration mFractalConfiguration = defaultFractalConfiguration;
-    bool mNeedsUpdate = true;
-    gl::Texture2dRef mFractalTexture;
-    bool mIsDragging = false;
-    vec2 mPrevMousePos;
-    bool mIsZooming = false;
-    int mFps = 0;
-    double mLastUpdateTime = 0.0;
+  bool isRunning;
+  double scale;
+  double zoomSpeed;
+  std::queue<std::pair<double, ci::Surface32f>> queue;
+  std::mutex queueMutex;
+  std::thread worker;
+  ci::Surface32f surface;
+
+  void workerLoop() {
+    while (isRunning) {
+      renderJulia(&surface, screenWidth, screenHeight, constantX, constantY,
+                  offsetX, offsetY, scale, maxIterations, escapeRadiusSquared);
+      {
+        std::lock_guard<std::mutex> lock(queueMutex);
+        queue.push(std::make_pair(scale, surface));
+
+        std::cout << "Rendered at scale: " << scale
+                  << ". Frame added to queue. Queue size: " << queue.size()
+                  << std::endl;
+      }
+      scale = scale + zoomSpeed;
+    }
+  }
 };
 
-void FractalApp::setup()
-{
-    setWindowSize(mScreenConfiguration.width, mScreenConfiguration.height);
-    ImGui::Initialize();
+class FractalApp : public ci::app::App {
+public:
+  FractalApp() : scale(1.0), zoomSpeed(0.01), renderer() {}
+
+  void setup() override;
+  void update() override;
+  void draw() override;
+
+private:
+  double scale;
+  double zoomSpeed;
+  Renderer renderer;
+  ci::gl::Texture2dRef texture;
+};
+
+void FractalApp::setup() {}
+
+void FractalApp::update() {
+  ci::Surface32f latestSurface;
+  renderer.getFrame(latestSurface, scale);
+  texture = ci::gl::Texture::create(latestSurface);
+  scale = scale + zoomSpeed;
 }
 
-void FractalApp::update()
-{
-    std::string constantName = mFractalConfiguration.constantName;
-    double constantX = mFractalConfiguration.constantX;
-    double constantY = mFractalConfiguration.constantY;
-    double offsetX = mFractalConfiguration.offsetX;
-    double offsetY = mFractalConfiguration.offsetY;
-    double scale = mFractalConfiguration.scale;
-    int maxIterations = mFractalConfiguration.maxIterations;
-    double escapeRadiusSquared = mFractalConfiguration.escapeRadiusSquared;
-
-    ImGui::Begin("Parameters");
-    ImGui::Text("Fractal configuration");
-    ImGui::InputDouble("Constant X", &constantX);
-    ImGui::InputDouble("Constant Y", &constantY);
-    ImGui::InputDouble("Offset X", &offsetX);
-    ImGui::InputDouble("Offset Y", &offsetY);
-    ImGui::InputDouble("Scale", &scale);
-    ImGui::InputInt("Max iterations", &maxIterations, 50);
-    ImGui::InputDouble("Escape radius squared", &escapeRadiusSquared);
-    if (ImGui::BeginCombo("Fractal Constants", constantName.c_str()))
-    {
-        for (const auto &[name, constant] : fractalConstants)
-        {
-            bool isSelected = (constantName == name);
-            if (ImGui::Selectable(name.c_str(), isSelected))
-            {
-                constantName = name;
-            }
-            if (isSelected)
-            {
-                ImGui::SetItemDefaultFocus();
-            }
-        }
-        ImGui::EndCombo();
-    }
-
-    ImGui::End();
-
-    if (constantX != mFractalConfiguration.constantX || constantY != mFractalConfiguration.constantY)
-    {
-        bool isInSet = false;
-        for (const auto &[name, constant] : fractalConstants)
-        {
-            if (constant.first == constantX && constant.second == constantY)
-            {
-                constantName = name;
-                isInSet = true;
-                break;
-            }
-        }
-        if (!isInSet)
-        {
-            constantName = "Custom";
-        }
-    }
-
-    if (constantName != mFractalConfiguration.constantName && constantName != "Custom")
-    {
-        constantX = fractalConstants.at(constantName).first;
-        constantY = fractalConstants.at(constantName).second;
-    }
-
-    if (constantX != mFractalConfiguration.constantX || constantY != mFractalConfiguration.constantY || offsetX != mFractalConfiguration.offsetX || offsetY != mFractalConfiguration.offsetY || scale != mFractalConfiguration.scale || maxIterations != mFractalConfiguration.maxIterations || escapeRadiusSquared != mFractalConfiguration.escapeRadiusSquared)
-    {
-        mFractalConfiguration.constantName = constantName;
-        mFractalConfiguration.constantX = constantX;
-        mFractalConfiguration.constantY = constantY;
-        mFractalConfiguration.offsetX = offsetX;
-        mFractalConfiguration.offsetY = offsetY;
-        mFractalConfiguration.scale = scale;
-        mFractalConfiguration.maxIterations = maxIterations;
-        mFractalConfiguration.escapeRadiusSquared = escapeRadiusSquared;
-        mNeedsUpdate = true;
-    }
-
-    if (mIsZooming)
-    {
-        mFractalConfiguration.scale *= 1.01f;
-        mNeedsUpdate = true;
-    }
-
-    // Calculate FPS
-    double currentTime = getElapsedSeconds();
-    double deltaTime = currentTime - mLastUpdateTime;
-    mLastUpdateTime = currentTime;
-    if (deltaTime > 0.0)
-    {
-        mFps = (int)(1.0f / deltaTime);
-    }
+void FractalApp::draw() {
+  ci::gl::clear(ci::Color(0, 0, 0));
+  ci::gl::draw(texture);
 }
 
-void FractalApp::draw()
-{
-    gl::clear(Color(0, 0, 0));
-    if (mNeedsUpdate)
-    {
-        Surface32f fractalSurface = renderFractal(mFractalConfiguration, mScreenConfiguration);
-        mFractalTexture = gl::Texture2d::create(fractalSurface);
-        mNeedsUpdate = false;
-    }
-    gl::draw(mFractalTexture);
-    ImGui::Begin("Performance");
-    ImGui::Text("FPS: %d", mFps);
-    ImGui::End();
-    ImGui::Render();
+void prepareSettings(FractalApp::Settings *settings) {
+  settings->setFrameRate(60);
+  settings->setWindowSize(screenWidth, screenHeight);
+  settings->setResizable(false);
 }
 
-void FractalApp::keyDown(ci::app::KeyEvent event)
-{
-    if (event.getChar() == 'q' || event.getChar() == 'Q')
-    {
-        quit();
-    }
-    else if (event.getChar() == 'r' || event.getChar() == 'R')
-    {
-        mFractalConfiguration = defaultFractalConfiguration;
-        mNeedsUpdate = true;
-        mIsZooming = false;
-    }
-    else if (event.getChar() == '+')
-    {
-        mFractalConfiguration.scale *= 1.1f;
-        mNeedsUpdate = true;
-    }
-    else if (event.getChar() == '-')
-    {
-        mFractalConfiguration.scale /= 1.1f;
-        mNeedsUpdate = true;
-    }
-    else if (event.getCode() == KeyEvent::KEY_LEFT)
-    {
-        mFractalConfiguration.offsetX -= 0.1f / mFractalConfiguration.scale;
-        mNeedsUpdate = true;
-    }
-    else if (event.getCode() == KeyEvent::KEY_RIGHT)
-    {
-        mFractalConfiguration.offsetX += 0.1f / mFractalConfiguration.scale;
-        mNeedsUpdate = true;
-    }
-    else if (event.getCode() == KeyEvent::KEY_UP)
-    {
-        mFractalConfiguration.offsetY -= 0.1f / mFractalConfiguration.scale;
-        mNeedsUpdate = true;
-    }
-    else if (event.getCode() == KeyEvent::KEY_DOWN)
-    {
-        mFractalConfiguration.offsetY += 0.1f / mFractalConfiguration.scale;
-        mNeedsUpdate = true;
-    }
-    else if (event.getCode() == KeyEvent::KEY_SPACE)
-    {
-        mIsZooming = !mIsZooming;
-        mNeedsUpdate = true;
-    }
-}
-
-void FractalApp::resize()
-{
-    mScreenConfiguration.width = getWindowWidth();
-    mScreenConfiguration.height = getWindowHeight();
-    mNeedsUpdate = true;
-}
-
-void FractalApp::mouseDown(MouseEvent event)
-{
-    mIsDragging = true;
-    mPrevMousePos = event.getPos();
-}
-
-void FractalApp::mouseDrag(MouseEvent event)
-{
-    if (mIsDragging)
-    {
-        vec2 currentMousePos = event.getPos();
-        vec2 delta = currentMousePos - mPrevMousePos;
-        mFractalConfiguration.offsetX -= delta.x / (mScreenConfiguration.width * mFractalConfiguration.scale);
-        mFractalConfiguration.offsetY -= delta.y / (mScreenConfiguration.height * mFractalConfiguration.scale);
-        mPrevMousePos = currentMousePos;
-        mNeedsUpdate = true;
-    }
-}
-
-void FractalApp::mouseWheel(MouseEvent event)
-{
-    float zoomFactor = event.getWheelIncrement();
-    mFractalConfiguration.scale *= (1.0f + zoomFactor * 0.1f);
-    mNeedsUpdate = true;
-}
-
-void FractalApp::mouseUp(MouseEvent event)
-{
-    mIsDragging = false;
-}
-
-void prepareSettings(FractalApp::Settings *settings)
-{
-    settings->setFrameRate(60);
-}
-
-CINDER_APP(FractalApp, RendererGl, prepareSettings)
+CINDER_APP(FractalApp, ci::app::RendererGl, prepareSettings)
